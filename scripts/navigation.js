570e8f9f74657599db3996f442444521034a01db
Olalekan Damola <olalekandamolaisaac@gmail.com>
2026-03-07 21:38:09 -0500

    Next mod

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,17 @@
 // Store the selected elements that we are going to use.
 const navbutton = document.querySelector('#ham-btn');
 const navlinks = document.querySelector('#nav-bar');
+const yearSpan = document.getElementById('year');
+const lastModifiedSpan = document.getElementById('lastModified');
+
+if (yearSpan) {
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+}
+
+if (lastModifiedSpan) {
+  lastModifiedSpan.textContent = document.lastModified;
+}
 
 // Toggle the show class off and on
 navbutton.addEventListener('click', () => {
