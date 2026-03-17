1e4721931e4980f755740b8bc1b5edd684e306a5
Olalekan Damola <olalekandamolaisaac@gmail.com>
2026-03-17 19:33:54 -0400

    Pushing next

---
+++ chamber/scripts/courses.js
@@ -0,0 +1,6 @@
+// Display current year in footer
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+// Display last modification date in footer
+document.querySelector("#lastModified").textContent =
+    "Last Modified: " + document.lastModified;
