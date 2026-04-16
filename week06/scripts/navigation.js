7db4c820411f59f19681b4efbae3634be19d81a9
Olalekan Damola <olalekandamolaisaac@gmail.com>
2026-04-16 14:29:12 -0400

    New commit

--- week06/scripts/navigation.js
+++ week06/scripts/navigation.js
@@ -1,10 +1,13 @@
 const hamBtn = document.querySelector("#ham-btn");
 const navBar = document.querySelector("#nav-bar");
 
+
+if (hamBtn && navBar) {
 hamBtn.addEventListener("click", () => {
     navBar.classList.toggle("show");
     hamBtn.classList.toggle("show");
 });
+}
 
 const links = document.querySelectorAll(".navigation a");
 
